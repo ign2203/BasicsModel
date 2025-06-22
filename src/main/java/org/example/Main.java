8d251b394148114321e7bbf8ab7ffc033c6b0a32
ign2203 <ign2203@yandex.ru>
2025-06-22 22:20:08 +0400

    Первый коммит

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] names = new String[100];
+        String[] phoneNumbers = new String[100];
+
+    }
+}
